d87335de7ded6cd3c1f11fc1daa0e4b83a986ddf
adi <tilis.tiadi@silentmode.my>
2023-07-31 23:01:50 +0700

    add about

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -9,7 +9,15 @@
 
 @section('content')
     <div class="body-content p-2 bg-white">
-
+        <div class="row" style="margin-top: 150px">
+            <div class="color-2" style="font-size: 32px; text-align:center; margin-bottom: 20px">KULTARA</div>
+            <div class="col-12 text-center">
+                <p class="p-5" style="font-style: italic">
+                    KULTARA atau singkatan dari Kuliner Nusantara merupakan aplikasi yang bertujuan untuk memberikan informasi tentang makanan atau minuman nusantara. Di aplikasi ini juga bisa melihat resep, review dan lokasi yang dibagikan oleh publik.
+                </p>
+                <div>Developer By <a href="http://github.com/ninjas007">Tilis Tiadi</a></div>
+            </div>
+        </div>
     </div>
 @endsection
 
