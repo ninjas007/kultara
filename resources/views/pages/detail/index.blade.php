3693f231da67a26bcc5b4c13737f85a48b64dfef
adi <tilis.tiadi@silentmode.my>
2023-07-31 23:51:48 +0700

    fix detail

--- resources/views/pages/detail/index.blade.php
+++ resources/views/pages/detail/index.blade.php
@@ -64,7 +64,7 @@
                     } else {
                         resultElement.innerHTML = `
                             <div class="text-muted mb-3" style="font-style: italic">Sumber: wikipedia</div>
-                            <h5 class="card-title">{{ $food->name }}</h5>
+                            <h5 class="card-title">{{ $food->nama }}</h5>
                             <p class="text-muted">{{ $food->keterangan_masakan }}</p>
                             <a href="https://id.wikipedia.org/wiki/{{ $food->name }}" target="_blank">Lihat Selengkapnya</a>
                             <br>
